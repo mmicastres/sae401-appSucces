dc8425f9a8d7b2556058690d0c66305edec6ff42
Joey <joeyfesta84@gmail.com>
2024-03-08 15:35:51 +0100

    Update api.php

--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -17,3 +17,37 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// Route de base
+
+Route::get('/', [Controller::class, 'homepage']);
+
+// Section connexion et utilisateurs
+
+Route::get('/login', [UserController::class, 'formLogin']);
+
+Route::post('/login', [UserController::class, 'login']);
+
+Route::get('/register', [UserController::class, 'formRegister']);
+
+Route::post('/register', [UserController::class, 'register']);
+
+Route::get('/user/{id}', [UserController::class, 'userInfo']);
+
+Route::get('/user/{id}/request', [UserController::class, 'friendRequest']);
+
+Route::get('/user/{id}/unfriend', [UserController::class, 'friendDelete']);
+
+Route::get('/account/modifier', [UserController::class, 'formModif']);
+
+Route::put('/account/modifier', [UserController::class, 'modif']);
+
+Route::delete('/account/delete', [UserController::class, 'deleteAccount']);
+
+// Section jeux
+
+Route::get('/jeux', [GameController::class, 'jeux']);
+
+Route::get('/jeux/{id}', [GameController::class, 'jeu']);
+
+Route::get('/jeux/{id}/favori', [GameController::class, 'jeuFavori']);
