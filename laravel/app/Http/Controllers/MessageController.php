a7069b0275cee8005185e8f7e9c7d32febdacf35
Joey <joeyfesta84@gmail.com>
2024-03-13 17:56:13 +0100

    Update MessageController.php

--- laravel/app/Http/Controllers/MessageController.php
+++ laravel/app/Http/Controllers/MessageController.php
@@ -34,14 +34,111 @@ class MessageController extends Controller
                 $participe->pseudo = $user->pseudo;
                 $participe::save();
             }
-            return response()->json(["status" => 1, "message" => "Le jeu a été ajouté aux favoris"], 201);
+            return response()->json(["status" => 1, "message" => "La conversation a été créé"], 201);
         } else {
             return response()->json(["status" => 0, "message" => "Erreur"], 400);
         }
     }
 
-    public function convContenu(Request $request, $id){
+    public function convContenu(Request $request, $id)
+    {
         $conversation = Conversation::with('messages')->find($id);
         return response()->json(["message" => "OK", "conversation" => $conversation]);
+    }
+
+    public function modifConv(Request $request, $id)
+    {
+        $conv = Conversation::find($id);
+        $conv->titre = $request->titre;
+        $ok = $conv->save();
+        if ($ok) {
+            if (!empty($request->participants)) {
+                $anciens = Participe::where('idConversation', $id)->get();
+                $anciens->delete();
+                foreach ($request->participants as $user) {
+                    $participe = new Participe;
+                    $participe->idConversation = $id;
+                    $participe->pseudo = $user->pseudo;
+                    $participe::save();
+                }
+            }
+            return response()->json(["status" => 1, "message" => "La conversation a été modifié"], 201);
+        } else {
+            return response()->json(["status" => 0, "message" => "Erreur"], 400);
+        }
+    }
+
+    public function deleteConv(Request $request, $id)
+    {
+        $anciens = Participe::where('idConversation', '=', $id)->get();
+        $anciens->delete();
+        $messages = Message::where('idConversation', '=', $id)->get();
+        foreach ($messages as $msg) {
+            $like = Likes::where('idMessage', $msg->idMessage)->get();
+            if ($like) {
+                $like->delete();
+            }
+        }
+        $messages->delete();
+        $conv = Conversation::find($id);
+        $ok = $conv->delete();
+        if ($ok) {
+            return response()->json(["status" => 1, "message" => "La conversation a été supprimée"], 201);
+        } else {
+            return response()->json(["status" => 0, "message" => "Erreur"], 400);
+        }
+    }
+
+    public function addMsg(Request $request, $id)
+    {
+        $msg = new Message;
+        $msg->content = $request->content;
+        // $msg->dateTime = ;
+        $msg->pseudo = Auth::user()->pseudo;
+        $msg->idConversation = $request->idConversation;
+        $ok = $msg->save();
+        if ($ok) {
+            return response()->json(["status" => 1, "message" => "Le message a été envoyé"], 201);
+        } else {
+            return response()->json(["status" => 0, "message" => "Erreur"], 400);
+        }
+    }
+
+    public function deleteMsg(Request $request, $id, $idmessage)
+    {
+        $msg = Message::find($idmessage);
+        $like = Likes::where('idMessage', $idmessage)->get();
+        if ($like) {
+            $like->delete();
+        }
+        $ok = $msg->delete();
+        if ($ok) {
+            return response()->json(["status" => 1, "message" => "Le message a été supprimé"], 201);
+        } else {
+            return response()->json(["status" => 0, "message" => "Erreur"], 400);
+        }
+    }
+
+    public function likeMsg(Request $request, $id, $idmessage)
+    {
+        $msg = Message::find($idmessage);
+        $like = Likes::where('idMessage', $idmessage)->where('pseudo', Auth::user()->pseudo)->get();
+        if($like){
+            $ok = $like->delete();
+            if ($ok) {
+                return response()->json(["status" => 1, "message" => "Votre like a été supprimé"], 201);
+            } else {
+                return response()->json(["status" => 0, "message" => "Erreur"], 400);
+            }
+        }else{
+            $like->idMessage = $idmessage;
+            $like->pseudo = Auth::user()->pseudo;
+            $ok = $like->save;
+            if ($ok) {
+                return response()->json(["status" => 1, "message" => "Votre like a été ajouté"], 201);
+            } else {
+                return response()->json(["status" => 0, "message" => "Erreur"], 400);
+            }
+        }
     }
 }
