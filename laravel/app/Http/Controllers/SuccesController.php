8f953958bff01ff8e58616454d7b405af2572238
kiliangui <kilianguilloteaux@gmail.com>
2024-03-14 11:33:54 +0100

    Authentification fonctionnel + début rooter

---
+++ laravel/app/Http/Controllers/SuccesController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SuccesController extends Controller
+{
+    //
+    public function succes(Request $request){
+
+    }
+    public function succesInfo(Request $request, $id){
+
+    }
+    public function succesComplete(Request $request, $id){
+
+    }
+    public function succesUnComplete(Request $request, $id){
+
+    }
+    public function succesNoter(Request $request, $id){
+
+    }
+}
