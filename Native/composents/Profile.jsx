708fdaaac5de0d6477e4e5b033755be1c0364678
Joey <joeyfesta84@gmail.com>
2024-04-25 15:04:45 +0200

    profile front

--- Native/composents/Profile.jsx
+++ Native/composents/Profile.jsx
@@ -1,29 +1,32 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 import { User } from "./User";
-import { View, Text,  Image, StyleSheet } from 'react-native';
-import { TextInput, IconButton, Card, Avatar,Button } from 'react-native-paper';
+import { View, Image, StyleSheet, ScrollView, FlatList, TouchableOpacity } from 'react-native';
+import { Text, TextInput, IconButton, Card, Avatar, Button, Title } from 'react-native-paper';
 
-export function Profile({ navigation,token,user,route="" }) {
-    let id =  "";
-    if (route !== ""){
+export function Profile({ navigation, token, user, route = "" }) {
+    let id = "";
+    if (route !== "") {
         id = route.params.id;
     }
-    console.log("id",id)
+    console.log("id", id)
     const [profile, setProfile] = useState();
     const [joueur, setJoueur] = useState([]);
     const [success, setSuccess] = useState([]);
     const [friends, setFriends] = useState([]);
     const [friendsRequests, setFriendsRequests] = useState([]);
     const [friendsRequestsSent, setFriendsRequestsSent] = useState([]);
-
+    console.log(friendsRequests)
+    console.log(friendsRequestsSent)
 
     useEffect(() => {
         if (id !== "" && id !== user.id) {
             console.log("NOT ME")
-            axios.get(process.env.EXPO_PUBLIC_API_URL + "/api/user/" + id,{headers: {
-                "Authorization": "Bearer " + token,
-            }}).then((response) => {
+            axios.get(process.env.EXPO_PUBLIC_API_URL + "/api/user/" + id, {
+                headers: {
+                    "Authorization": "Bearer " + token,
+                }
+            }).then((response) => {
                 setSuccess(response.data.succes);
                 setFriends(response.data.friends);
                 setJoueur(response.data.joueur);
@@ -36,7 +39,7 @@ export function Profile({ navigation,token,user,route="" }) {
             if (!user || !user.id) return;
             console.log("ME")
             console.log("USER ID", user["id"])
-            axios.get(process.env.EXPO_PUBLIC_API_URL + "/api/user/" + user.id,{
+            axios.get(process.env.EXPO_PUBLIC_API_URL + "/api/user/" + user.id, {
                 headers: {
                     "Authorization": "Bearer " + token,
                 }
@@ -56,7 +59,7 @@ export function Profile({ navigation,token,user,route="" }) {
     }, [user]);
 
     function ajoutAmi() {
-        axios.post(process.env.EXPO_PUBLIC_API_URL + "/api/user/" + id + "/friend",{},{
+        axios.post(process.env.EXPO_PUBLIC_API_URL + "/api/user/" + id + "/friend", {}, {
             headers: {
                 "Authorization": "Bearer " + token,
             }
@@ -85,105 +88,136 @@ export function Profile({ navigation,token,user,route="" }) {
         });
     }
 
-    return (<>
-        <View style={{ margin: 5, flexDirection: 'row', justifyContent: 'flex-start', alignItems: 'baseline' }}>
-            <View style={{ flexDirection: 'column', alignItems: 'center', justifyContent: 'center', width: '25%', marginRight: 5 }}>
+    return (<ScrollView>
+        <View style={{ margin: 5, }}>
+            <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'space-around' }}>
                 {profile ? (
-                    <Avatar.Image size={64} source={{ uri: `http://localhost:8000/imgprofile/${profile.picture}` }} />
+                    <Avatar.Image size={128} source={{ uri: `http://localhost:8000/imgprofile/${profile.picture}` }} />
                 ) : null}
-                {!id || user?.id === id ? (
-                    <View style={{ flexDirection: 'row', justifyContent: 'center' }}>
-                        <Button mode="contained" style={{ marginRight: 10 }}>Choisir un fichier</Button>
-                        <Button mode="contained" onPress={handlePdp}>Envoyer</Button>
-                    </View>
-                ) : null}
+                <Text variant="displayMedium">{profile?.pseudo}</Text>
             </View>
             <View style={{ flexDirection: 'column', justifyContent: 'center', width: '25%' }}>
-                <Text style={{ fontSize: 24, marginBottom: 10 }}>{profile?.pseudo}</Text>
-                {id !== "" && id !== user.id && (
+                {id !== "" && id !== user.id ? (
                     <Button mode="contained" style={{ width: '50%' }} onPress={ajoutAmi} >
                         {profile?.isFriendRequestSent ? "En attente d'ajout" :
                             profile?.isFriendRequest ? "Accepter la demande d'ami" :
                                 profile?.isFriend ? "Retirer l'ami" : "Ajouter l'ami"}
                     </Button>
-                )}
+                ) : <></>}
             </View>
         </View>
-    <View style={{ flexDirection: 'row' }}>
-        <View style={{ margin: 5, width: '25%' }}>
-            <Text>Jeux favoris :</Text>
-            <View>
-                {joueur.filter(item => item.favori === 1).map(item => (
-                    <View style={{ marginBottom: 10 }} key={item.idJeu}>
-                        <Image source={{ uri: item.jeu.capsule }} style={{ width: 50, height: 50 }} />
-                        <Text>{item.nom}</Text>
-                    </View>
-                ))}
-            </View>
-            <Text>Jeux actifs :</Text>
-            <View>
-                {joueur.filter(item => item.actif === 1).map(item => (
-                    <View style={{ marginBottom: 10 }} key={item.idJeu}>
-                        <Image source={{ uri: item.jeu.capsule }} style={{ width: 50, height: 50 }} />
-                        <Text>{item.nom}</Text>
-                    </View>
-                ))}
-            </View>
-            <Text>Jeux possédés :</Text>
-            <View>
-                {joueur.filter(item => item.possede === 1).map(item => (
-                    <View style={{ marginBottom: 10 }} key={item.idJeu}>
-                        <Image source={{ uri: item.jeu.capsule }} style={{ width: 50, height: 50 }} />
-                        <Text>{item.nom}</Text>
-                    </View>
-                ))}
-            </View>
-        </View>
-        <View style={{ flexDirection: 'column', alignItems: 'center', width: '75%' }}>
-            <View>
-                <Text style={{ fontSize: 18, marginBottom: 10 }}>Nombre de succès : {success.length}</Text>
-                <View style={{ flexDirection: 'row', flexWrap: 'wrap' }}>
-                    {success.map(item => (
-                        <Card key={item.idSucces} style={{ padding: 10, marginBottom: 10 }}>
-                            <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-                                <Image source={{ uri: `https://achievementstats.com/${item.iconUnlocked}` }} style={{ marginRight: 10, width: 50, height: 50 }} />
-                                <View>
-                                    <Text>{item.nom}</Text>
-                                    <Text>{item.description}</Text>
-                                </View>
-                            </View>
+
+        <View style={{ flex: 1, margin: 5 }}>
+            <Text variant="titleLarge" style={styles.title}>Jeux favoris :</Text>
+            <FlatList
+                data={joueur.filter((item) => item.favori === 1)}
+                renderItem={({ item }) => (
+                    <TouchableOpacity onPress={() => navigation.navigate('Jeu', { idJeu: item.idJeu })}>
+                        <Card style={{ margin: 8 }}>
+                            <Card.Cover source={{ uri: `https://cdn.cloudflare.steamstatic.com/steam/apps/${item.jeu.steamId}/header.jpg` }} />
+                            <Card.Content style={{ alignItems: "center" }}>
+                                <Title>{item.jeu.nom}</Title>
+                            </Card.Content>
                         </Card>
-                    ))}
-                </View>
-            </View>
-            <View>
-                <Text>Amis</Text>
-                <View>
-                    {friends.map(item => (
-                        <User friend={user.id === id} key={item.id} user={item} />
-                    ))}
-                </View>
+                    </TouchableOpacity>
+                )}
+                keyExtractor={(item) => item.idJeu.toString()}
+                numColumns={1}>
+            </FlatList>
+
+            <Text variant="titleLarge" style={styles.title}>Jeux actifs :</Text>
+            <FlatList
+                data={joueur.filter((item) => item.actif === 1)}
+                renderItem={({ item }) => (
+                    <TouchableOpacity onPress={() => navigation.navigate('Jeu', { idJeu: item.idJeu })}>
+                        <Card style={{ margin: 8 }}>
+                            <Card.Cover source={{ uri: `https://cdn.cloudflare.steamstatic.com/steam/apps/${item.jeu.steamId}/header.jpg` }} />
+                            <Card.Content style={{ alignItems: "center" }}>
+                                <Title>{item.jeu.nom}</Title>
+                            </Card.Content>
+                        </Card>
+                    </TouchableOpacity>
+                )}
+                keyExtractor={(item) => item.idJeu.toString()}
+                numColumns={1}>
+            </FlatList>
+
+            <Text variant="titleLarge" style={styles.title}>Jeux possédés :</Text>
+            <FlatList
+                data={joueur.filter((item) => item.possede === 1)}
+                renderItem={({ item }) => (
+                    <TouchableOpacity onPress={() => navigation.navigate('Jeu', { idJeu: item.idJeu })}>
+                        <Card style={{ margin: 8 }}>
+                            <Card.Cover source={{ uri: `https://cdn.cloudflare.steamstatic.com/steam/apps/${item.jeu.steamId}/header.jpg` }} />
+                            <Card.Content style={{ alignItems: "center" }}>
+                                <Title>{item.jeu.nom}</Title>
+                            </Card.Content>
+                        </Card>
+                    </TouchableOpacity>
+                )}
+                keyExtractor={(item) => item.idJeu.toString()}
+                numColumns={1}>
+            </FlatList>
+        </View>
+
+        <View style={styles.container}>
+            <Text variant="titleLarge" style={styles.title}>Nombre de succès : {success.length}</Text>
+            <FlatList
+                style={{ marginTop: 10 }}
+                data={success}
+                renderItem={({ item }) => (
+                    <TouchableOpacity onPress={() => navigation.navigate('Succes', { idSucces: item.idSucces })}>
+                        <Card key={item.idSucces} style={styles.card}>
+                            <Card.Title
+                                title={item.nom}
+                                left={() => <Avatar.Image size={48} source={{ uri: "https://achievementstats.com/" + item.iconUnlocked }} />}
+                            />
+                            <Card.Content>
+                                <Text variant="bodyMedium">{item.description}</Text>
+                            </Card.Content>
+                        </Card>
+                    </TouchableOpacity>
+                )}
+                keyExtractor={(item) => item.idSucces.toString()}
+                numColumns={1}>
+            </FlatList>
+        </View>
+        <View>
+            <View style={styles.container}>
+                <Text variant="titleLarge" style={styles.title}>Amis</Text>
+                <FlatList
+                    style={{ marginTop: 10 }}
+                    data={friends}
+                    renderItem={({ item }) => (
+                        <User friend={user.id === id} key={item.id} user={item} onPress={() => navigation.navigate(`Profile`, { id: item.id })} />
+                    )}
+                    keyExtractor={(item) => item.id.toString()}
+                    numColumns={1}>
+                </FlatList>
             </View>
             {!id || user?.id === id ? (
                 <>
-                    <Text>Friends requests</Text>
-                    <View>
-                        {friendsRequests?.map(item => (
-                            <User key={item.id} user={item} />
-                        ))}
-                    </View>
-                    <Text>Friends requests sent</Text>
-                    <View>
-                        {friendsRequestsSent?.map(item => (
-                            <User key={item.id} user={item} />
-                        ))}
-                    </View>
+                    {friendsRequests.length != 0 ? (
+                        <View style={styles.container}>
+                            <Text>Demandes d'amis reçues</Text>
+                            {friendsRequests?.map(item => (
+                                <User key={item.id} user={item} />
+                            ))}
+                        </View>
+                    ) : <Text>Pas de demandes d'amis en attente</Text>}
+                    {friendsRequestsSent.length != 0 ? (
+                        <View style={styles.container}>
+                            <Text>Demandes d'amis envoyées</Text>
+                            {friendsRequestsSent?.map(item => (
+                                <User key={item.id} user={item} />
+                            ))}
+                        </View>
+                    ) : <Text>Pas de demandes d'amis envoyées</Text>}
                 </>
             ) : null}
         </View>
-    </View>
-    </>
-);
+    </ScrollView>
+    );
 
 
 }
@@ -191,6 +225,7 @@ export function Profile({ navigation,token,user,route="" }) {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+        flexDirection: 'column',
         padding: 20,
         justifyContent: 'center',
         alignItems: 'center',
@@ -208,4 +243,11 @@ const styles = StyleSheet.create({
         fontWeight: 'bold',
         marginBottom: 10,
     },
+    title: {
+        alignItems: 'center'
+    },
+    card: {
+        marginBottom: 10,
+        width: 340
+    }
 });
